bb154673b4bebbbe67b23bb96208f52f7a6ab684
adsiitam08 <36767352+adsiitam08@users.noreply.github.com>
2018-04-20 09:22:46 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -4,13 +4,15 @@
  */
 public class Solution {
     public boolean problema(String s){
+        if(s == "")
+            return false;
         String subs;
         String concat;
         for(int i = 1; i<s.length(); i++){
             subs = s.substring(0, i);
             concat = subs;
             while(concat.length()<=s.length()){
-                System.out.println(subs);
+                //System.out.println(subs);
                 if(concat.equals(s))
                     return true;
                 concat = concat + subs;
@@ -22,26 +24,26 @@ public class Solution {
     public static void main(String[] args){
         Solution sol = new Solution();
         
-        System.out.print(sol.problema("abab"));
-        System.out.print(sol.problema("abba"));
-        System.out.print(sol.problema("gugus"));
-        System.out.print(sol.problema("gusugs"));
-        System.out.print(sol.problema("gusgus"));
-        System.out.print(sol.problema("gustavogustavogustavo"));
-        System.out.print(sol.problema("pruebapruebas"));
-        System.out.print(sol.problema("ahhhhhhhhhhhhhhhha"));
-        System.out.print(sol.problema("hhhhhhh"));
-        System.out.print(sol.problema("a___a"));
-        System.out.print(sol.problema("a_aaa_a"));
-        System.out.print(sol.problema("holaalo"));
-        System.out.print(sol.problema("hholahola"));
-        System.out.print(sol.problema("12341234"));
-        System.out.print(sol.problema("12_21_12"));
-        System.out.print(sol.problema("prueba"));
-        System.out.print(sol.problema("okokok"));
-        System.out.print(sol.problema("nelnol"));
-        System.out.print(sol.problema("simonsimon"));
-        System.out.print(sol.problema("okasdokas"));
+        System.out.print(sol.problema("abab")); //true
+        System.out.print(sol.problema("abba")); //false
+        System.out.print(sol.problema("gugus")); //false
+        System.out.print(sol.problema("gusugs")); //false
+        System.out.print(sol.problema("gusgus")); //true
+        System.out.print(sol.problema("gustavogustavogustavo")); //true
+        System.out.print(sol.problema("pruebapruebas")); //false
+        System.out.print(sol.problema("ahhhhhhhhhhhhhhhha")); //false
+        System.out.print(sol.problema("hhhhhhh")); //true
+        System.out.print(sol.problema("a___a")); //false
+        System.out.print(sol.problema("a_aaa_a")); //false
+        System.out.print(sol.problema("holaalo")); //false
+        System.out.print(sol.problema("hholahola")); //false
+        System.out.print(sol.problema("12341234")); //true
+        System.out.print(sol.problema("12_21_12")); //false
+        System.out.print(sol.problema("prueba")); //false
+        System.out.print(sol.problema("okokok")); //true
+        System.out.print(sol.problema("nelnol")); //false
+        System.out.print(sol.problema("simonsimon")); //true
+        System.out.print(sol.problema("okasdokas")); //false
         
     }
 }
